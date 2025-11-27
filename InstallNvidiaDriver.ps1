6e34597b254dcda5dd66146d5c6aa0e85198c9b4
Thomas Edvardsen <ThomasEdvardsen@users.noreply.github.com>
2025-11-27 21:06:31 +0100

    Update InstallNvidiaDriver.ps1

--- InstallNvidiaDriver.ps1
+++ InstallNvidiaDriver.ps1
@@ -1,23 +1,27 @@
 # Hent driverinfo om nyeste NVIDIA GeForce Game Ready (DCH, WHQL) for Windows 11 â€“ UK English
 $r = Invoke-RestMethod 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=127&pfid=1023&osID=135&languageCode=1078&beta=0&isWHQL=0&dltype=-1&dch=1&upCRD=0&qnf=0&sort1=1&numberOfResults=1'
 $info = $r.IDS[0].downloadInfo
-$exeUrl = $info.DownloadURL
-if (-not $exeUrl) { throw "DownloadURL mangler i responsen." }
+$version = $info.Version
+$downloadUrl = $info.DownloadURL
+if (-not $downloadUrl) { throw "DownloadURL mangler i responsen." }
 
-$filename = Split-Path $exeUrl -Leaf
+$filename = Split-Path $downloadUrl -Leaf
 $dst = Join-Path $env:TEMP $filename
 
-Write-Host "Versjon: $info.Version"
-Write-Host "Fil:     $filename"
-Write-Host "URL:     $exeUrl"
+Write-Host "Versjon: GeForce Game Ready Driver $version"
+Write-Host "URL:     $downloadUrl"
 
-Write-Host "Laster ned med BITS til $dst ..."
-Start-BitsTransfer -Source $exeUrl -Destination $dst
+$old = $ProgressPreference
+$ProgressPreference = 'SilentlyContinue'
 
-Write-Host "Installerer driver stille..."
-#Start-Process $dst -ArgumentList "/s /noreboot" -Wait
+Write-Host "Downloading driver $version to $dst ..."
+Invoke-WebRequest -Uri "$downloadUrl" -OutFile "$dst"
+$ProgressPreference = $old
 
-Write-Host "Rydder opp..."
+Write-Host "Installing driver silent..."
+$proc = Start-Process -FilePath $dst -ArgumentList "-s -noreboot" -Wait -PassThru
+
+#Write-Host "Cleaning up..."
 #Remove-Item $dst -Force
 
-Write-Host "Ferdig."
+Write-Host "Finished."
